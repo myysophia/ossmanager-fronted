7cd465852dd3a2520a25354cb937f4541fa59db4
王九日 <wangxucs123@126.com>
2025-03-27 16:28:52 +0800

    fix: fix env NEXT_PUBLIC_API_URL

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api/v1';
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/api/v1/:path*',
+        destination: `${apiUrl}/:path*`,
       },
     ];
   },
